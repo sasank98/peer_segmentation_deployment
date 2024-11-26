e9eda4bbfe45a22447f29757e9e4a877743b91ba
sasank98 <102756754+sasank98@users.noreply.github.com>
2024-11-26 03:16:28 -0600

    changes to solve ultralytics.boxes issue

--- peer_ws/src/pallet_detection_python/pallet_detection_python/pallet_detection.py
+++ peer_ws/src/pallet_detection_python/pallet_detection_python/pallet_detection.py
@@ -11,34 +11,34 @@ class PalletDetectionNode(Node):
         super().__init__('pallet_detection_python')
         self.subscription = self.create_subscription(
             Image,
-            '/robot1/zed2i/left/image_rect_color',
+            '/rgb/image_rect_color',
             self.listener_callback,
             10)
         # subscribes to the left camera image of zed2i,topic obtained from zed2i ROS2 documentation
-        
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") # Puts the model on the GPU if available
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # Puts the model on the GPU if available
         self.publisher = self.create_publisher(Image, '/modified_image', 10) # publishes to /modified_image topic
         self.bridge = CvBridge()
         
-        self.model = YOLO("/home/peer_ws/src/pallet_detection_python/pallet_detection_python/best.pt").device(self.device)
+        self.model = YOLO("/home/peer_ws/src/pallet_detection_python/pallet_detection_python/best.pt") # not a good idea, [COMPOSITION]
 
         self.class_dict = {0: 'ground', 1: 'pallet'}
 
     def listener_callback(self, msg):
+        print('Received an image')
         cv_image = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
-        results = self.model(cv_image)
+        result_numpy = self.model(cv_image)[0].cpu().numpy() # convert output to cpu and numpy arrays for opencv processing
         # couldn't test this due to lack of example data, if this doesnt work, try to convert the results to cpu and numpy array
         # results = results.cpu().numpy()
         # given code is faster than converting to numpy array
-        for *box, conf, cls in results.xyxy[0]:
-        
-            if cls == 1:
-                cv2.rectangle(cv_image, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), 2)
-                cv2.putText(cv_image, self.class_dict[int(cls)], (int(box[0]), int(box[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        for i in range(result_numpy.boxes.cls.shape[0]):
+            xyxy = result_numpy.boxes.xyxy[i]
+            if result_numpy.boxes.cls[i] == 0:
+                cv2.rectangle(cv_image, (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3])), (0, 255, 0), 2)
+                cv2.putText(cv_image, self.class_dict[int(result_numpy.boxes.cls[i])], (int(xyxy[0]), int(xyxy[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             else:
-                cv2.rectangle(cv_image, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 0, 255), 2)
-                cv2.putText(cv_image, self.class_dict[int(cls)], (int(box[0]), int(box[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        
+                cv2.rectangle(cv_image, (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3])), (0, 0, 255), 2)
+                cv2.putText(cv_image, self.class_dict[int(result_numpy.boxes.cls[i])], (int(xyxy[0]), int(xyxy[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                
         modified_image_msg = self.bridge.cv2_to_imgmsg(cv_image, 'bgr8')
         self.publisher.publish(modified_image_msg)
 
